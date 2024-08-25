8659994797e1782eaacdddcc120cc06bd5c47675
성현우 <yannoo@naver.com>
2024-08-26 01:51:00 +0900

    구글맵 키 잘 적용됨

--- cagong_googlemap/ios/Runner/AppDelegate.swift
+++ cagong_googlemap/ios/Runner/AppDelegate.swift
@@ -1,13 +1,14 @@
-import Flutter
 import UIKit
-
+import Flutter
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+override func application(
+_ application: UIApplication,
+didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+) -> Bool {
+GeneratedPluginRegistrant.register(with: self)
+GMSServices.provideAPIKey("AIzaSyD9lyWXaDm86LMhTaPtlHSXK4mYH5Wt0bg")
+return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+}
 }
--- cagong_googlemap/lib/screens/map_screen.dart
+++ cagong_googlemap/lib/screens/map_screen.dart
@@ -1,135 +1,66 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'bottom_sheet_content.dart'; // 바텀 시트 UI를 위한 파일
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MapScreen(),
-    );
-  }
-}
+import 'dart:async';
 
 class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  GoogleMapController? _controller;
-  final Set<Marker> _markers = {};
-
-  @override
-  void initState() {
-    super.initState();
-    _loadMarkersFromJson();
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(37.5665, 126.9780); // 서울의 위도와 경도
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
   }
 
-  Future<void> _loadMarkersFromJson() async {
-    // assets에서 JSON 파일을 불러옵니다.
-    String jsonData = await rootBundle.loadString('assets/json/cafe_info.json');
-    final List<dynamic> data = jsonDecode(jsonData);
+  void _addMarker(LatLng position) {
+    final MarkerId markerId = MarkerId(position.toString());
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: position,
+      infoWindow: InfoWindow(title: '선택한 위치', snippet: '${position.latitude}, ${position.longitude}'),
+    );
 
-    // JSON 데이터를 기반으로 마커를 생성합니다.
-    for (var item in data) {
-      _markers.add(
-        Marker(
-          markerId: MarkerId(item['ID'].toString()),
-          position: LatLng(item['Position (Latitude)'], item['Position (Longitude)']),
-          infoWindow: InfoWindow(title: item['Name']),
-          onTap: () {
-            _showBottomSheet(
-              context,
-              item['Name'],
-              item['Message'],
-              item['Address'],
-              item['Price'],
-              item['영업 시간'],
-              item['Video URL'],
-              item['Seating Type 1'],
-              item['Seating Count 1'],
-              item['Power Count 1'],
-              item['Seating Type 2'],
-              item['Seating Count 2'],
-              item['Power Count 2'],
-              item['Seating Type 3'],
-              item['Seating Count 3'],
-              item['Power Count 3'],
-            );
-          },
-        ),
-      );
-    }
-
-    // 상태를 갱신하여 마커를 지도에 표시합니다.
-    setState(() {});
+    setState(() {
+      markers[markerId] = marker;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Map with Bottom Sheet'),
+        title: Text('지도'),
       ),
       body: GoogleMap(
-        onMapCreated: (GoogleMapController controller) {
-          _controller = controller;
-        },
+        onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
-          target: LatLng(37.5867, 126.9963), // 첫 번째 카페 위치로 초기 카메라 위치 설정
-          zoom: 15,
+          target: _center,
+          zoom: 11.0,
         ),
-        markers: _markers,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        markers: Set<Marker>.of(markers.values),
+        onTap: _addMarker,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.center_focus_strong),
+        onPressed: () {
+          _goToCenter();
+        },
       ),
     );
   }
 
-  void _showBottomSheet(
-      BuildContext context,
-      String name,
-      String message,
-      String address,
-      String price,
-      String hours,
-      String videoUrl,
-      String? seatingType1,
-      double? seatingCount1,
-      String? powerCount1,
-      String? seatingType2,
-      double? seatingCount2,
-      String? powerCount2,
-      String? seatingType3,
-      double? seatingCount3,
-      String? powerCount3,
-      ) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return BottomSheetContent(
-          name: name,
-          message: message,
-          address: address,
-          price: price,
-          hours: hours,
-          videoUrl: videoUrl,
-          seatingType1: seatingType1,
-          seatingCount1: seatingCount1,
-          powerCount1: powerCount1,
-          seatingType2: seatingType2,
-          seatingCount2: seatingCount2,
-          powerCount2: powerCount2,
-          seatingType3: seatingType3,
-          seatingCount3: seatingCount3,
-          powerCount3: powerCount3,
-        );
-      },
-    );
+  Future<void> _goToCenter() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: _center, zoom: 11),
+    ));
   }
 }
