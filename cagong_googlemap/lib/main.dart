f48d5ef66481da50c74236ae1f341734c39e178c
성현우 <yannoo@naver.com>
2024-09-06 18:30:38 +0900

    파이어베이스 추가중
    파이어베이스 관련 이것저것 넣는중

--- cagong_googlemap/lib/main.dart
+++ cagong_googlemap/lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'utils/app_router.dart';
 import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart' as web;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // Firebase Auth 명시적 초기화
+    await FirebaseAuth.instance.authStateChanges().first;
+  } catch (e) {
+    print('Firebase 초기화 오류: $e');
+    // 여기에 오류 처리 로직을 추가할 수 있습니다.
+  }
+
+
   if (kIsWeb) {
     GoogleMapsFlutterPlatform.instance = web.GoogleMapsPlugin();
   }
+  
   runApp(const MyApp());
 }
 
