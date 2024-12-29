e772fdba5de133a8054640c8eee06631521a3eb6
성현우 <yannoo@naver.com>
2024-12-29 20:20:25 +0900

    Revert "마커 사이즈"
    This reverts commit 4e1e3fe087ee5541fd6f4436c69e83c0942b73ea.

--- cagong_googlemap/lib/utils/marker_generator_web.dart
+++ cagong_googlemap/lib/utils/marker_generator_web.dart
@@ -6,8 +6,6 @@ import '../models/cafe.dart';
 import 'dart:typed_data';
 
 class PlatformMarkerGenerator {
-  static final Map<String, BitmapDescriptor> _markerCache = {};
-
   static Future<BitmapDescriptor> generateMarker({
     required Cafe cafe,
     required String markerImagePath,
@@ -15,74 +13,54 @@ class PlatformMarkerGenerator {
     required double fontSize,
     required double maxTextWidth,
   }) async {
-    final String cacheKey = '${cafe.id}_$markerImagePath';
-    if (_markerCache.containsKey(cacheKey)) {
-      return _markerCache[cacheKey]!;
-    }
     try {
-      bool isMobile = (html.window.innerWidth ?? 800) < 600;
-      double scaleFactor = isMobile ? 0.7 : 1.0;
-
-      double dpr = (html.window.devicePixelRatio ?? 1.0).toDouble();
-
-      final webImagePath = '${html.window.location.origin}/$markerImagePath';
-
+      final webImagePath = html.window.location.origin + '/' + markerImagePath.replaceFirst('assets', 'assets');
+      
       final htmlImage = html.ImageElement()
         ..src = webImagePath
         ..crossOrigin = 'anonymous';
 
       final completer = Completer<BitmapDescriptor>();
-
+      
       htmlImage.onLoad.listen((_) {
-        double adjustedMarkerSize = markerSize * scaleFactor * dpr;
-        double adjustedFontSize = fontSize * scaleFactor * dpr;
-        final width = (adjustedMarkerSize * 1.8).toInt(); // 마커 너비 증가(글씨 잘려서ㅠ)
-        final height = (adjustedMarkerSize + adjustedFontSize + 4).toInt();
-
+        final width = (markerSize * 1.8).toInt(); // 마커 너비 증가(글씨 잘려서ㅠ)
+        final height = (markerSize + fontSize + 4).toInt();
+        
         final canvas = html.CanvasElement(width: width, height: height)
-          ..style.width = '${width / dpr}px'
-          ..style.height = '${height / dpr}px';
-
+          ..style.width = '${width}px'
+          ..style.height = '${height}px';
+        
         final ctx = canvas.context2D;
-
+        
         ctx.clearRect(0, 0, width, height);
-
+        
         // 마커 이미지는 원래 크기로 중앙에 배치
-        final imageX = (width - adjustedMarkerSize) / 2;
+        final imageX = (width - markerSize) / 2;
         ctx.drawImageScaledFromSource(
-            htmlImage,
-            0,
-            0,
-            htmlImage.naturalWidth,
-            htmlImage.naturalHeight,
-            imageX,
-            0,
-            adjustedMarkerSize,
-            adjustedMarkerSize);
-
+          htmlImage,
+          0, 0, htmlImage.naturalWidth!, htmlImage.naturalHeight!,
+          imageX, 0, markerSize, markerSize
+        );
+        
         // 텍스트 크기 조정
-        final adaptiveFontSize =
-            cafe.name.length > 5 ? adjustedFontSize * 0.8 : adjustedFontSize;
+        final adaptiveFontSize = cafe.name.length > 5 ? fontSize * 0.8 : fontSize;
 
         ctx.font = 'bold ${adaptiveFontSize}px Arial';
         ctx.fillStyle = 'black';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'top';
-        ctx.fillText(cafe.name, width / 2, adjustedMarkerSize + 2, width - 10);
-
+        ctx.fillText(cafe.name, width / 2, markerSize + 2, width - 10);
+        
         final dataUrl = canvas.toDataUrl('image/png');
         final data = _dataUrlToUint8List(dataUrl);
-
-        final bitmapDescriptor = BitmapDescriptor.fromBytes(data);
-        _markerCache[cacheKey] = bitmapDescriptor;
-
+        
         completer.complete(BitmapDescriptor.fromBytes(data));
       });
 
       htmlImage.onError.listen((event) {
         completer.completeError('Failed to load image: $webImagePath');
       });
-
+      
       return completer.future;
     } catch (e) {
       print('Error generating marker: $e');
