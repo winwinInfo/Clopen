418a0d9b451ad4bad1a8ff3f208cf02623fe8f7f
성현우 <yannoo@naver.com>
2025-04-05 23:45:05 +0900

    seperate cluster marker

--- cagong_googlemap/lib/screens/map_screen.dart
+++ cagong_googlemap/lib/screens/map_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import '../models/cafe.dart';
 import '../utils/custom_marker_generator.dart';
+import '../utils/cluster_marker.dart';
 import '../utils/location_marker.dart';
 import '../widgets/search_bar.dart' as custom_search_bar;
 import '../widgets/bottom_sheet.dart';
@@ -23,6 +24,7 @@ class MapScreen extends StatefulWidget {
   MapScreenState createState() => MapScreenState();
 }
 
+
 class MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller = Completer();
   static const LatLng _center = LatLng(37.5665, 126.9780);
@@ -35,27 +37,40 @@ class MapScreenState extends State<MapScreen> {
   StreamSubscription<Position>? _positionStreamSubscription;
   double _bottomSheetHeight = 0;
 
-  late ClusterManager<Cafe> _clusterManager;
-
+  //현위치 마커 서비스 인스턴스
   final LocationMarkerService _locationService = LocationMarkerService();
+  // 클러스터 마커 서비스 인스턴스
+  final ClusterMarkerService _clusterService = ClusterMarkerService();
 
 
   @override
   void initState() {
     super.initState();
-    // ClusterManager 초기화
-    _clusterManager = ClusterManager<Cafe>(
-      [],
-      _updateMarkers,
-      markerBuilder: _markerBuilder,
-    );
 
     // 위치 서비스 초기화 및 콜백 등록
     _initLocationService();
+    // 클러스터 마커 초기화
+    _initClusterService();
     // 카페 마커 load
     _loadCafesAndCreateMarkers();
   }
 
+  // 클러스터 서비스 초기화
+  void _initClusterService() {
+    _clusterService.initClusterManager(_updateMarkers);
+
+    // 클러스터 탭 이벤트 처리
+    _clusterService.onClusterTap = (cafes) {
+      showClusterBottomSheet(context, cafes);
+    };
+
+    // 카페 탭 이벤트 처리
+    _clusterService.onCafeTap = (cafe) {
+      _handleCafeSelected(cafe);
+    };
+  }
+
+  //현위치 서비스 초기화
   Future<void> _initLocationService() async {
     await _locationService.initializeCircleMarker();
 
@@ -98,9 +113,9 @@ class MapScreenState extends State<MapScreen> {
       List<dynamic> jsonResponse = json.decode(jsonString);
       _cafes = jsonResponse.map((data) => Cafe.fromJson(data)).toList();
 
-      // ClusterManager에 카페 추가
-      _clusterManager.setItems(_cafes);
-      _clusterManager.updateMap();
+      _clusterService.setItems(_cafes);
+      _clusterService.updateMap();
+
       if (mounted) {
         setState(() {
           _isLoading = false;
@@ -114,11 +129,10 @@ class MapScreenState extends State<MapScreen> {
   //필터 적용
   void _applyFilters() {
     List<Cafe> filteredCafes = _filterManager.applyFilters(_cafes);
-    _clusterManager.setItems(filteredCafes);
-    _clusterManager.updateMap();
+    _clusterService.setItems(filteredCafes);
+    _clusterService.updateMap();
   }
 
-  // 추가: _updateMarkers 메서드
   void _updateMarkers(Set<Marker> markers) {
     if (!mounted) return;
     setState(() {
@@ -134,38 +148,6 @@ class MapScreenState extends State<MapScreen> {
     });
   }
 
-  // 클러스터링 하면서 추가된 마커빌더
-  Future<Marker> _markerBuilder(dynamic cluster) async {
-    if (cluster.isMultiple) {
-      // 클러스터 마커 처리
-      return Marker(
-        markerId: MarkerId(cluster.getId()),
-        position: cluster.location,
-        onTap: () {
-          print('Cluster tapped with ${cluster.count} items');
-          showClusterBottomSheet(context, cluster.items);
-          // 클러스터 탭 처리 로직
-        },
-        icon: await _getClusterMarker(cluster.count),
-      );
-    } else {
-      // 단일 마커 처리
-      final cafe = cluster.items.first;
-      final markerIcon = await CustomMarkerGenerator.createCustomMarker(
-        cafe,
-        markerSize: 36,
-        fontSize: 14,
-        maxTextWidth: 400,
-      );
-      return Marker(
-        markerId: MarkerId('${cafe.latitude},${cafe.longitude}'),
-        position: LatLng(cafe.latitude, cafe.longitude),
-        icon: markerIcon,
-        onTap: () => _handleCafeSelected(cafe),
-        anchor: const Offset(0.5, 0.5),
-      );
-    }
-  }
 
   void showClusterBottomSheet(BuildContext context, List<Cafe> cafes) {
     // 바텀 시트 열기
@@ -229,61 +211,6 @@ class MapScreenState extends State<MapScreen> {
     }
   }//showClusterBottomSheet
 
-Future<BitmapDescriptor> _getClusterMarker(int clusterSize) async {
-  // MediaQuery를 사용하여 현재 디바이스의 픽셀 비율 얻기
-  final dpr = WidgetsBinding.instance.window.devicePixelRatio;
-  
-  // 기본 크기를 픽셀 비율로 나누어 조정
-  final baseSize = 60 + (clusterSize * 0.3).clamp(0, 40);
-  final adjustedSize = baseSize / dpr;
-  final adjustedFontSize = (18 + (clusterSize * 0.1).clamp(0, 14)) / dpr;
-
-  final recorder = ui.PictureRecorder();
-  final canvas = Canvas(recorder);
-  final paint = Paint()
-    ..color = Colors.brown
-    ..style = PaintingStyle.fill;
-
-  // 조정된 크기로 그리기
-  canvas.drawCircle(
-    Offset(adjustedSize / 2, adjustedSize / 2), 
-    adjustedSize / 2, 
-    paint
-  );
-
-  final textPainter = TextPainter(
-    textDirection: TextDirection.ltr,
-    textAlign: TextAlign.center,
-    text: TextSpan(
-      text: clusterSize.toString(),
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: adjustedFontSize,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-  );
-
-  textPainter.layout();
-  textPainter.paint(
-    canvas,
-    Offset(
-      adjustedSize / 2 - textPainter.width / 2, 
-      adjustedSize / 2 - textPainter.height / 2
-    ),
-  );
-
-  final image = await recorder.endRecording().toImage(
-    adjustedSize.toInt(), 
-    adjustedSize.toInt()
-  );
-  final data = await image.toByteData(format: ui.ImageByteFormat.png);
-
-  return BitmapDescriptor.fromBytes(data!.buffer.asUint8List());
-}
-
-
-
 
 //화면 구성
   bool _isBottomSheetOpen = false;
@@ -311,7 +238,7 @@ Future<BitmapDescriptor> _getClusterMarker(int clusterSize) async {
                   : GoogleMap(
                       onMapCreated: (GoogleMapController controller) {
                         _controller.complete(controller);
-                        _clusterManager.setMapId(controller.mapId);
+                        _clusterService.setMapId(controller.mapId);
                       },
                       initialCameraPosition: const CameraPosition(
                         target: _center,
@@ -321,8 +248,8 @@ Future<BitmapDescriptor> _getClusterMarker(int clusterSize) async {
                       myLocationButtonEnabled: false,
                       zoomControlsEnabled: false,
                       markers: _markers,
-                      onCameraMove: _clusterManager.onCameraMove,
-                      onCameraIdle: _clusterManager.updateMap,
+                      onCameraMove: _clusterService.onCameraMove,
+                      onCameraIdle: _clusterService.updateMap,
                     ),
             ),
             Positioned(
@@ -376,6 +303,7 @@ Future<BitmapDescriptor> _getClusterMarker(int clusterSize) async {
       ),
     );
   }
+
 
 //when cafe selected
   void _handleCafeSelected(Cafe selectedCafe) async {
